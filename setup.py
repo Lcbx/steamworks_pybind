00f6f113884c12024454f13919677168ed5b7d9f
Lcbx <luc.courbariaux@gmail.com>
2026-06-13 17:12:00 +0200

    version 0.1.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if not destination.exists(): copy_file(origin, destination)
 
 setup(
 	name='steamworks',
-	version='0.1',
+	version='0.1.1',
 	author='Lcbx',
 	author_email='https://github.com/Lcbx',
 	description='WIP procedural bindings for steamworks',
